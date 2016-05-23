d5b78c944abe7754a5e8c06a58b7a35fc95b2e09
yezhennan <yezhennan@cooee.cn>
2016-05-23 11:31:37 +0800

    【H5GameCenter】1、Android 6.0读写外置存储器权限问题：需要在代码里提醒用户打开对应权限。 2、Android 6.0通知栏进度条高度适配。

--- H5GameCenter/src/main/java/com/cooeeui/h5gamecenter/main/H5GameCenterActivity.java
+++ H5GameCenter/src/main/java/com/cooeeui/h5gamecenter/main/H5GameCenterActivity.java
@@ -1,6 +1,11 @@
 package com.cooeeui.h5gamecenter.main;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
@@ -19,6 +24,18 @@ import com.cooeeui.h5gamecenter.basecore.views.NumberProgressBar;
 public class H5GameCenterActivity extends BaseActivity {
 
     private final String TAG = H5GameCenterActivity.class.getSimpleName();
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private String mDLApkUrl;
+    private String mDLPackageName;
+    private String mApkName;
+    private String mNotiIconBase64;
 
     private final int PROGRESSBAR_MIN = 10;
     private final int BACKKEY_RESPONSE_TIME = 5000;
@@ -91,14 +108,65 @@ public class H5GameCenterActivity extends BaseActivity {
         }
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                DLNotificationManager.getInstance(H5GameCenterActivity.this)
+                    .startDLNotification(mDLApkUrl, mDLPackageName, mApkName, mNotiIconBase64);
+            } else {
+                Toast.makeText(getApplicationContext(), "PERMISSION_DENIED", Toast.LENGTH_SHORT)
+                    .show();
+            }
+        }
+    }
+
     public class JavaScriptObject {
 
         @JavascriptInterface
         public void downloadGame(String apkurl, String apkPackageName, String apkName,
                                  String notiIconBase64Str) {
-            DLNotificationManager.getInstance(H5GameCenterActivity.this)
-                .startDLNotification(apkurl, apkPackageName, apkName, notiIconBase64Str);
+            if (Build.VERSION.SDK_INT >= 23) {
+                mDLApkUrl = apkurl;
+                mDLPackageName = apkPackageName;
+                mApkName = apkName;
+                mNotiIconBase64 = notiIconBase64Str;
+                if (verifyStoragePermissions(H5GameCenterActivity.this)) {
+                    DLNotificationManager.getInstance(H5GameCenterActivity.this)
+                        .startDLNotification(apkurl, apkPackageName, apkName, notiIconBase64Str);
+                }
+            } else {
+                DLNotificationManager.getInstance(H5GameCenterActivity.this)
+                    .startDLNotification(apkurl, apkPackageName, apkName, notiIconBase64Str);
+            }
         }
 
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     */
+    public static boolean verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat
+            .checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            );
+            return false;
+        }
+
+        return true;
     }
 }
